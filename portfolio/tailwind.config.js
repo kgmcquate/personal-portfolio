cc42260993058bb7ff14cea971be0b4fc49ac910
Kevin McQuate <kgmcquate@gmail.com>
2023-09-08 08:24:10 -0500

    reduced padding for mobile

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -4,16 +4,16 @@ module.exports = {
   ],
   important: '#root',
   theme: {
-    screens: {
-      'tablet': '640px',
-      // => @media (min-width: 640px) { ... }
+    // screens: {
+    //   'tablet': '640px',
+    //   // => @media (min-width: 640px) { ... }
 
-      'laptop': '1024px',
-      // => @media (min-width: 1024px) { ... }
+    //   'laptop': '1024px',
+    //   // => @media (min-width: 1024px) { ... }
 
-      'desktop': '1280px',
-      // => @media (min-width: 1280px) { ... }
-    },
+    //   'desktop': '1280px',
+    //   // => @media (min-width: 1280px) { ... }
+    // },
     fontFamily: {
       display: ['Open Sans', 'sans-serif']
     },
